c4a5e89d01d58fc6e41d63d0143ad580fad7c71d
Justin Hill <justinmhill@gmail.com>
2015-06-17 07:25:33 -0400

    Update user schema fields

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -4,7 +4,21 @@ var Schema = mongoose.Schema;
 var UserSchema = new Schema({
 	firstName: String,
 	lastName: String,
-	emailAddress: String
+	emailAddress: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	password: String,
+	bucketList: [{
+		type: Schema.ObjectId,
+		ref: 'BucketList'
+
+	}],
+	created: {
+		type: Date,
+		default: Date.now
+	}
 });
 
 mongoose.model('User', UserSchema);
