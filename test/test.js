707730af0d0cb4d6def1783313efe2b9022b0269
Justin Hill <justinmhill@gmail.com>
2015-06-17 07:47:27 -0400

    add test for request to places

--- test/test.js
+++ test/test.js
@@ -9,3 +9,15 @@ var should = chai.should();
 var app = server.app;
 
 chai.use(chaiHttp);
+
+describe('Bucket List of Destinations', function(){
+	it('should list destinations on GET request', function(done){
+		chai.request(app).
+			.get('/list/places')
+			.end(function(err, res) {
+				res.should.be(json);
+				res.body.should.be.a('array');
+				done();
+			});
+	});
+});
