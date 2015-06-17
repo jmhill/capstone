a0ca9a0fdf6e9b1c5d838dfb481b065c22f08f13
Justin Hill <justinmhill@gmail.com>
2015-06-17 05:38:13 -0400

    Add bucketList controller file

---
+++ app/controllers/bucketList.server.controller.js
@@ -0,0 +1,9 @@
+var bucketList = require('mongoose').model('BucketList');
+
+exports.addPlace = function() {
+	// add a new place/destination to the bucketlist
+};
+
+exports.list = function() {
+	// list out the different place objects in the bucketlist
+};
