7fb942b835c0697534cc394bf5fadf5ff4e1f2de
Justin Hill <justinmhill@gmail.com>
2015-06-18 19:30:48 -0400

    Add simple ejs template view

--- config/express.js
+++ config/express.js
@@ -5,5 +5,11 @@ var express = require('express');
 
 module.exports = function() {
 	var app = express();
+
+	app.set('views', './app/views');
+	app.set('view engine', 'ejs');
+
+	require('../app/routes/index.server.routes')(app);
+	
 	return app;
-}
+};
