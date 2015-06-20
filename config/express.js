80c5b638acbed9631cf44ff65015d12409e98740
Justin Hill <justinmhill@gmail.com>
2015-06-19 22:19:08 -0400

    Add morgan logger for dev env

--- config/express.js
+++ config/express.js
@@ -2,9 +2,16 @@
 
 var config = require('./config');
 var express = require('express');
+var morgan = require('morgan');
 
 module.exports = function() {
 	var app = express();
+	
+	if (process.env.NODE_ENV === 'development') {
+		app.use(morgan('dev'));
+	} else if (process.env.NODE_ENV === 'production') {
+		app.use(compress());
+	}
 
 	app.set('views', './app/views');
 	app.set('view engine', 'ejs');
