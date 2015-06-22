953dbbead2458c8a094517bc515b8bfd251b3cc0
Justin Hill <justinmhill@gmail.com>
2015-06-22 19:52:51 -0400

    Add signup page templating and routes

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -9,4 +9,8 @@ module.exports = function(app) {
 	app.route('/signin')
 		.get(users.renderSignin)
 		.post( /* passport authentication */ );
+
+	app.route('/signup')
+		.get(users.renderSignup)
+		.post();
 };
