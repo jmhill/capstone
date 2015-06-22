f3c8bf3e8ccf6976bb2b922023478b32107a25bb
Justin Hill <justinmhill@gmail.com>
2015-06-22 19:41:01 -0400

    Add signin page templating
    The signin page is rendered on the backend with EJS, and served up via
    a renderSignin method attached to the user controller.

---
+++ app/controllers/users.server.controller.js
@@ -0,0 +1,33 @@
+var User = require('mongoose').model('User');
+
+exports.create = function(req, res, next) {
+	var user = new User(req.body);
+
+	user.save(function(err) {
+		if (err) {
+			return next(err);
+		} else {
+			res.json(user);
+		}
+	});
+};
+
+exports.list = function(req, res, next) {
+	User.find({}, function(err, users) {
+		if (err) {
+			return next(err);
+		} else {
+			res.json(users);
+		}
+	});
+};
+
+exports.renderSignin = function(req, res, next) {
+	if (!req.user) {
+		res.render('pages/signin', {
+			title: 'Sign-in Form',
+		});
+	} else {
+		return res.redirect('/');
+	}
+}
