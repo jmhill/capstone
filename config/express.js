1bb29184119d34cc8e39ee7cfdb0254be589d1fd
Justin Hill <justinmhill@gmail.com>
2015-06-22 20:51:22 -0400

    Move javascript main file and enable express.static

--- config/express.js
+++ config/express.js
@@ -16,6 +16,8 @@ module.exports = function() {
 	app.set('views', './app/views');
 	app.set('view engine', 'ejs');
 
+	app.use(express.static('public'));
+
 	require('../app/routes/index.server.routes')(app);
 	require('../app/routes/users.server.routes')(app);
 	
