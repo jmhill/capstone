a1c99b8d1b1cec6f224fcfd1f7be5bd903848727
Justin Hill <justinmhill@gmail.com>
2015-08-21 10:46:18 -0400

    Update comments in express config/initialization file

--- config/express.js
+++ config/express.js
@@ -11,6 +11,7 @@ var passport = require('passport');
 module.exports = function() {
 	var app = express();
 
+	// If in dev environment, use morgan logger to display http requests in server console.
 	if (process.env.NODE_ENV === 'development') {
 		app.use(morgan('dev'));
 	} else if (process.env.NODE_ENV === 'production') {
@@ -21,22 +22,31 @@ module.exports = function() {
 	app.set('views', './app/views');
 	app.set('view engine', 'ejs');
 
+	// Use body parser middleware to decode json data
 	app.use(bodyParser.urlencoded({
 		extended: true
 	}));
 	app.use(bodyParser.json());
 
+	// Use express-session middleware to handle session cookies and store session
+	// data on server.
+	// TODO: Research and implement a session store. express-session defaults to using
+	// a new MemoryStore, which is not intended for production environment.
 	app.use(session({
 		saveUninitialized: true,
 		resave: true,
 		secret: config.sessionSecret
 	}));
 
+	// Set up passportJS to handle user authentication.
 	app.use(passport.initialize());
 	app.use(passport.session());
 
+	// Enable loading of static files from public directory.
 	app.use(express.static('public'));
 
+	// Routing middleware is set up in individual modules and must be
+	// 'registered' here to function in the app.
 	require('../app/routes/index.server.routes')(app);
 	require('../app/routes/users.server.routes')(app);
 	require('../app/routes/places.server.routes')(app);
